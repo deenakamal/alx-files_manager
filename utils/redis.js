122f3a8fc07c7993f7ab5be449694fab37c819ae
deenakamal <deenah.kamal@gmail.com>
2024-09-01 19:25:54 -0700

    create redis

---
+++ utils/redis.js
@@ -0,0 +1,57 @@
+import redis from 'redis';
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        
+        this.client.on('error', (err) => {
+            console.error('Redis error:', err);
+        });
+        
+        this.client.on('ready', () => {
+            console.log('Redis client connected');
+        });
+    }
+    /**
+     * Checks if this client's connection to the Redis server is active.
+     */    
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err, reply) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(reply);
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
